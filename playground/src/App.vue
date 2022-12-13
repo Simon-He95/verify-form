35203e9d38188a29739fc5844f85fe2551c8e0a9
Simon He <13917107469@163.com>
2022-12-13 12:40:41 +0800

    chore: update

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,35 +1,34 @@
 <script setup lang="ts">
-const formEl = ref(null)
+const formEl = ref(null);
 const formData = reactive({
-  age: '',
-  name: '',
-  country: '',
-  address: '',
+  age: "",
+  name: "",
+  country: "",
+  address: "",
   rules: {
     age: [
-      /* required */ val => !!val || 'Required',
-      /* number */ val => !isNaN(Number(val)) || 'Expected number',
-      /* length */ val =>
-        val <= 18 ? 'You are too young' : val >= 100 ? 'You are too old' : '',
+      /* required */ (val) => !!val || "Required",
+      /* number */ (val) => !isNaN(Number(val)) || "Expected number",
+      /* length */ (val) =>
+        val <= 18 ? "You are too young" : val >= 100 ? "You are too old" : "",
     ],
     name: [
-      /* required */ val => !!val || 'Required',
-      /* length */ val => val.length > 2 || 'Length needs to be more than 2',
+      /* required */ (val) => !!val || "Required",
+      /* length */ (val) => val.length > 2 || "Length needs to be more than 2",
     ],
   },
-})
+});
 
 function submit() {
   /* eslint no-alert: "off" */
   if (formEl.value.getStatus()) {
-    alert('Submission successful')
-  }
-  else {
+    alert("Submission successful");
+  } else {
     const msg = formEl.value
       .getErrorMsg()
       .map(({ prop, errorMsg }) => `${prop} is ${errorMsg}`)
-      .join('\n')
-    alert(msg)
+      .join("\n");
+    alert(msg);
   }
 }
 </script>
@@ -39,8 +38,9 @@ function submit() {
     <VividTyping
       content="Verify Form"
       :infinity="true"
-      :interval="500"
+      :interval="200"
       :stable="true"
+      spilt-tag="span"
       text-5xl
       m-t-10
       m-b-20
@@ -54,27 +54,21 @@ function submit() {
         @submit.prevent="submit"
       >
         <VerifyFormField label="年龄" prop="age" required>
-          <input v-model="formData.age" type="text">
+          <input v-model="formData.age" type="text" />
         </VerifyFormField>
         <VerifyFormField label="姓名" prop="name" required>
-          <input v-model="formData.name" type="text">
+          <input v-model="formData.name" type="text" />
         </VerifyFormField>
         <VerifyFormField label="国籍" prop="country">
-          <input v-model="formData.country" type="text">
+          <input v-model="formData.country" type="text" />
         </VerifyFormField>
         <VerifyFormField label="地址" prop="address">
-          <input v-model="formData.address" type="text">
+          <input v-model="formData.address" type="text" />
         </VerifyFormField>
         <div flex="~ gap-5" justify-center items-center m-t-5 w-full>
-          <button btn type="submit">
-            提交
-          </button>
-          <button btn type="button" @click="formEl.$reset()">
-            清除错误
-          </button>
-          <button btn type="button" @click="formEl.$clear()">
-            清空数据
-          </button>
+          <button btn type="submit">提交</button>
+          <button btn type="button" @click="formEl.$reset()">清除错误</button>
+          <button btn type="button" @click="formEl.$clear()">清空数据</button>
         </div>
       </VerifyForm>
     </div>
